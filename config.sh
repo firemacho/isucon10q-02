c782ede5513793b248375cd2299ca635299e9256
Nagamine Ryota <nicebirdyryota@gmail.com>
2024-11-23 19:31:48 +0900

    config.shのSSH設定を更新

--- config.sh
+++ config.sh
@@ -4,15 +4,11 @@
 ## 何も起動しない場合は""を指定する
 declare -A SERVER_SERVICES=(
   ["s1"]="app mysql nginx"
-  ["s2"]="app mysql nginx"
-  ["s3"]="app mysql nginx"
 )
 
 # 各サーバにSSHで接続する際のホスト名
 declare -A SERVER_HOSTS=(
   ["s1"]="server1"
-  ["s2"]="server2"
-  ["s3"]="server3"
 )
 # SSHのユーザ
 ssh_user=isucon
